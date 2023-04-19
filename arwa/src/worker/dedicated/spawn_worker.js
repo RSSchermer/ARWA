e5cc020b74d541115e6351dfb28b8c6329195517
RSSchermer <roland0507@gmail.com>
2023-04-19 22:43:34 +0200

    Working example of dedicated worker spawning
    Came accross the missing piece in https://github.com/chemicstry/wasm_thread . It uses a clever hack with a stack trace to obtain the name of the wasm-bindgen shim script, thus allowing the worker to setup the missing WASM module's imports correctly.

--- arwa/src/worker/dedicated/spawn_worker.js
+++ arwa/src/worker/dedicated/spawn_worker.js
@@ -1,29 +1,6 @@
-export function spawn_worker(module, memory, pointer) {
-    const WORKER_SCRIPT = `
-        let initialize = function(messageEvent) {
-            let { module, memory, pointer } = messageEvent.data;
-            
-            let imports = { wbg: { memory } };
-            
-            WebAssembly.instantiate(module, imports).then(function(instance) {
-                instance.exports.__arwa_init_spawned_worker(pointer);
-            });
-  
-            // Remove the initializer after first event so we don't collide with user-defined 
-            // message listeners
-            removeEventListener("message", initialize);
-        }
-        
-        addEventListener("message", initialize);
-    `;
-
+export function spawn_worker(script, module, memory, pointer) {
     // Create worker
-    let worker = new Worker(URL.createObjectURL(new Blob(
-        [WORKER_SCRIPT],
-        {type: 'application/javascript'}
-    )));
-
-    console.log(memory);
+    let worker = new Worker(script, { type: "module" });
 
     // Initialize worker with the spawn closure
     worker.postMessage({
